7fca1305e2ae095afd45e4a89da4950f57fdeabe
Igor <gosha1308@gmail.com>
2020-04-14 08:26:06 +0300

    Сделан личный кабинет

--- lk.php
+++ lk.php
@@ -3,37 +3,86 @@
   if(!$_SESSION['id']) {
     header('Location: auth.php');
   }
+  $title = "Личный кабинет";
+  $style  = '
+            td {
+              width:100%;
+              margin-top: 50px;
+              }
+              .fa-pencil, .fa-check, fa-times {
+                cursor: pointer;
+              }
+            ';
+  require_once('site_components/header.php');
 ?>
 
-<!doctype html>
-<html lang="ru">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
- 
-    <title>Личный кабинет</title>
-  </head>
-  <body>
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-6">
           <h1 class = "text-center mt-5">Личный кабинет</h1>
           <h4>Здравствуйте,  <span><?php echo $_SESSION['name'] ?></span></h4>
+          <table>
+            <tr>
+              <td>Логин </td>
+              <td class = "data" data = "login"><?php echo $_SESSION['login'] ?></td>
+              <td>
+                <span class = "edit">
+                  <i class="fa fa-pencil" aria-hidden="true"></i>
+                </span>
+                <span class = "startedit d-none">
+                  <i class="fa fa-check" aria-hidden="true"></i>
+                  <i class="fa fa-times" aria-hidden="true"></i>
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <td>Фамилия </td>
+              <td><?php echo $_SESSION['lastname'] ?></td>
+            </tr>
+            <tr>
+              <td>Имя </td>
+              <td><?php echo $_SESSION['name'] ?></td>
+            </tr>
+            <tr>
+              <td>Отчество </td>
+              <td><?php echo $_SESSION['patronymic'] ?></td>
+            </tr>
+            <tr>
+              <td>Дата рождения </td>
+              <td><?php echo $_SESSION['birthdate'] ?></td>
+            </tr>
+
+          </table>   
         </div>
       </div>
     </div>
-   
-
-    <!-- Optional JavaScript -->
-    <script src="https://use.fontawesome.com/9aea11037f.js"></script>
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     
-  </body>
-</html>
+    <script>
+      let pens = document.querySelectorAll('.fa-pencil');
+      for (let i = 0; i < pens.length; i++) {
+        pens[i].onclick = startEdit;
+      } 
+      function startEdit() {
+        let buttonsTd = this.parentElement.parentElement;
+        let dataTd = buttonsTd.previousElementSibling;
+        
+        let editSpan = buttonsTd.querySelector('.edit');
+        let startEditSpan = buttonsTd.querySelector('.startedit');
+        
+        startEditSpan.classList.remove('d-none');
+        editSpan.classList.add('d-none');
+        
+        let okBtn = buttonsTd.querySelector('.fa-check');
+        let denyBtn = buttonsTd.querySelector('.fa-check');
+        
+        denyBtn.onclick = () => {
+          startEditSpan.classList.add('d-none');
+          editSpan.classList.remove('d-none');
+        }
+        
+      //  console.log([buttonsTd, dataTd]);
+      }
+    </script>
+    
+  <?php require_once('site_components/footer.php'); ?>
+
